58687fae12e86e0c5df100276376ca02c9fd6c46
Kazuki Ohta <k_ota28@hotmail.com>
2016-07-16 22:13:19 +0900

    プロジェクト ファイルを追加します。

---
+++ LightweightPrismSample/App.xaml.cs
@@ -0,0 +1,19 @@
+﻿using LightweightPrismSample.Views;
+using Microsoft.Practices.Unity;
+using Prism.Mvvm;
+using System.Windows;
+
+namespace LightweightPrismSample
+{
+    public partial class App : Application
+    {
+        // アプリで管理するコンテナ
+        private IUnityContainer Container { get; } = new UnityContainer();
+
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            ViewModelLocationProvider.SetDefaultViewModelFactory(x => this.Container.Resolve(x));
+            this.Container.Resolve<MainWindow>().Show();
+        }
+    }
+}
---
+++ LightweightPrismSample/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,27 @@
+﻿using Prism.Commands;
+using Prism.Interactivity.InteractionRequest;
+using Prism.Mvvm;
+
+namespace LightweightPrismSample.ViewModels
+{
+    public class MainWindowViewModel : BindableBase
+    {
+        public DelegateCommand AlertCommand { get; }
+
+        public InteractionRequest<INotification> AlertRequest { get; } = new InteractionRequest<INotification>();
+
+        private string input;
+
+        public string Input
+        {
+            get { return this.input; }
+            set { this.SetProperty(ref this.input, value); }
+        }
+
+        public MainWindowViewModel()
+        {
+            this.AlertCommand = new DelegateCommand(() =>
+                this.AlertRequest.Raise(new Notification { Title = "Alert", Content = this.Input }));
+        }
+    }
+}
